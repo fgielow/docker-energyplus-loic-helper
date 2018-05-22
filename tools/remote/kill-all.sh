bea0b7b0f6c2172b9c2cbc7bf07f518f88157b86
Fernando Gielow <fgielow@arbnco.com>
2018-05-22 12:33:13 +0100

    correct kill all

--- tools/remote/kill-all.sh
+++ tools/remote/kill-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ps aux | grep EnergyPlus | awk '{print $2}' | xargs kill -9
+eval `echo kill -9 $(ps aux | grep parallel-process | awk '{print $2}')`
 
-ps aux | grep parallel-process.sh | awk '{print $2}' | xargs kill -9
+sleep 1
+
+eval `echo kill -9 $(ps aux | grep EnergyPlus | awk '{print $2}')`
